957281f324d4539b0b4a66ce7bbe809f5c8c5486
Adam Cardenas <cevaris@gmail.com>
2019-04-17 06:32:22 -0700

    api can query arbritrary data

--- api/main.go
+++ api/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"cloud.google.com/go/datastore"
 	"fmt"
+	"github.com/cevaris/status/report"
+	"github.com/cevaris/timber"
 	"log"
 	"net/http"
 	"os"
@@ -9,8 +12,11 @@ import (
 	"google.golang.org/appengine"
 )
 
+var logger = timber.NewAppEngineLogger()
+
 func main() {
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/reports.json", getReports)
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -27,5 +33,37 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	fmt.Fprint(w, "Hello, World!")
+	_, err := fmt.Fprint(w, "Hello, World!")
+	if err != nil {
+		fmt.Println("failed to write", err)
+	}
+}
+
+
+func getReports(w http.ResponseWriter, r *http.Request) {
+	projectID := os.Getenv("PROJECT_ID")
+	ctx := appengine.NewContext(r)
+	dsClient, err := datastore.NewClient(ctx, projectID)
+	if err != nil {
+		panic(err)
+	}
+
+
+	keys := []*datastore.Key {
+		datastore.NameKey("ApiReportMin", "aws_us_west_2_s3_read_file:1555340160", nil),
+		datastore.NameKey("ApiReportMin", "aws_us_west_2_s3_read_file:1555340220", nil),
+	}
+
+	var reports = make([]report.ApiReport, 2)
+	err = dsClient.GetMulti(ctx, keys, reports)
+	if err != nil {
+		panic(err)
+	}
+
+	logger.Info(ctx, "count", fmt.Sprintf("%+v", reports))
+
+	_, writeErr := fmt.Fprint(w, reports)
+	if writeErr != nil {
+		fmt.Println("failed to write", writeErr)
+	}
 }
